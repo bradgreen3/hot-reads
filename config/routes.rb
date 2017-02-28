49f5e25268bbd981ec37d526220e1ea3bf6bf9d9
Brad <bgreen3@uoregon.edu>
2017-02-27 17:13:37 -0700

    Add links display on root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'links#index'
+
   namespace :api do
     namespace :v1 do
       resources :links, only: [:create]
     end
   end
+
+  resources :links, only: [:index]
 end

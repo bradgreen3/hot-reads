ff5d06a1a3e43a09fd3f71171761cf14c6fbfd88
Brad <bgreen3@uoregon.edu>
2017-02-27 21:24:24 -0700

    Add endpoint for receiving hot reads

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -1,6 +1,15 @@
 class Api::V1::LinksController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  def index
+    @links = Link.where(updated_at: (Time.now - 24.hours)..Time.now).limit(10).group(:url).order('count_id DESC').count(:id)
+    @hot_links = []
+    @links.map do |url, count|
+      @hot_links << url
+    end
+    render :json => @hot_links.to_json, status => 200
+  end
+
   def create
     @link = Link.create(url: params["url"])
     render :json => @link.to_json, :status => 200
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,5 +3,5 @@ class LinksController < ApplicationController
   def index
     @links = Link.where(updated_at: (Time.now - 24.hours)..Time.now).limit(10).group(:url).order('count_id DESC').count(:id)
   end
-  
+
 end

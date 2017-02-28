f1b45ff57e0c761b42d7df2438ffe82aec8b79a8
Brad <bgreen3@uoregon.edu>
2017-02-28 14:08:18 -0700

    Move logic out of links controller

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -2,11 +2,7 @@ class Api::V1::LinksController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    @links = Link.where(updated_at: (Time.now - 24.hours)..Time.now).limit(10).group(:url).order('count_id DESC').count(:id)
-    @hot_links = []
-    @links.map do |url, count|
-      @hot_links << url
-    end
+    @hot_links = Link.hot
     render :json => @hot_links.to_json, status => 200
   end
 
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,10 @@
 class Link < ApplicationRecord
+  def self.hot
+    @links = Link.where(updated_at: (Time.now - 24.hours)..Time.now).limit(10).group(:url).order('count_id DESC').count(:id)
+    @hot_links = []
+    @links.map do |url, count|
+      @hot_links << url
+    end
+    @hot_links
+  end
 end
